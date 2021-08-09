34cce465cb92906c9d222fae5dbb2cc36138372e
Solomon Ekrebe <krebedev@gmail.com>
2021-08-09 21:36:53 +0100

    update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,97 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
 import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import * as styles from './../styles/homepage.module.css'
+import Layout from '../components/layout'
+import * as styles from './../styles/index.module.css'
+import Button from './../components/button';
+import { StaticImage } from 'gatsby-plugin-image'
+import ProjectCards from '../components/project-cards';
+import BlogCards from '../components/blog-cards'
+import Cta from '../components/cta'
 
-const IndexPage = ({ data }) => {
+const HomePage = ({ data }) => {
 	const { name, jobTitle } = data.site.siteMetadata.author
 	return (
 		<Layout>
 			<Helmet title={`${jobTitle} | ${name}`} />
-			<header className={styles.header}>
-				<h1 className={styles.headerTitle}>
-					Hi, my name is Solomon E
-					<span className={styles.highlight}>krebe</span>. <br /> I'm an avid
-					learner and Frontend <span className={styles.highlight}>Dev</span>
-					eloper
-				</h1>
-				<p className={styles.headerIntro}>
-					I build elegant and effective web / mobile apps that deliver great
-					user experiences.
-				</p>
-				<p className={styles.headerIntro}>
-					I believe in an accessible web and digital ecosystem and adapt my
-					craft to meet these salient needs.
-				</p>
-			</header>
+			<section className={styles.hero}>
+			<div className={`container flex-centered ${styles.heroInner}`}>
+			<article className={styles.heroCopy}>
+		
+			<h1 className={styles.heroHeading}>{name} <br />
+				<span className={styles.highlight}>{jobTitle}</span>
+			</h1>
+			<p>
+				I build elegant and efficient websites / mobile apps that deliver great
+				user experiences.
+			</p>
+		
+			<div className={styles.heroCta}>
+			<Button pathname={'/about'} title={'More About me'} />
+			</div>
+		</article>
+
+		<div className={styles.heroImage}>
+			<StaticImage src='./../images/me.jpg'
+				alt='krebeDev - Solomon Ekrebe'
+				placeholder='tracedSVG'
+				width={350}
+				height={350}
+				className={styles.avatar}
+			 />
+		</div>
+		</div>
+	</section>
+			<section className={styles.projects}>
+			<div className={`container`}>
+				<h2>Here's a set of my recent projects</h2>
+				<p>With some tools and tech stacks I have dabbled with.</p>
+				<ProjectCards projects={data.allMdx.nodes} />
+				<Button pathname={'/portfolio'} title={'View all Projects'} />
+			</div>
+			</section>
+			<section>
+				<div className={`container`}>
+				<h2>I started writing for the web of recent</h2>
+				<p>Checkout my  latest posts.</p>
+				<BlogCards />
+				<Button pathname={'/blog'} title={'See all Posts'} />
+				</div>
+			</section>
+			<Cta />
 		</Layout>
 	)
 }
 
-export default IndexPage
+export default HomePage
 
 export const query = graphql`
-	query HomePageQuery {
-		site {
-			siteMetadata {
-				author {
-					jobTitle
-					name
-				}
-			}
-		}
-	}
+query HomePageQuery {
+  site {
+    siteMetadata {
+      author {
+        jobTitle
+        name
+      }
+    }
+  }
+  allMdx(
+		filter: {frontmatter: {featuredProject: {gte: 0}}}
+    sort: {fields: frontmatter___featuredProject, order: ASC}
+  ) {
+    nodes {
+      frontmatter {
+        title
+        tags
+				featuredImage {
+          childImageSharp {
+            gatsbyImageData(width: 600)
+          }
+        }
+      }
+			slug
+			id
+    }
+  }
+}
 `
