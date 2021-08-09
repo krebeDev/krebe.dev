60c2643bbc8998d05487ce9a9afd5ec326a695a7
Solomon Ekrebe <krebedev@gmail.com>
2021-08-09 21:42:12 +0100

    create custom icons

---
+++ src/components/close-icon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CloseIcon = (props) => {
+  return ( <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48.828 48.828"><g fill="none" stroke="#dddfe1" strokeWidth="5"><path d="M1.414 1.414l46 46M47.414 1.414l-46 46"/></g></svg>)
+}
+ 
+export default CloseIcon;
