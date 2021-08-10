735ca3ec826a9bcc7b917943e60a0104a06dc207
Solomon Ekrebe <krebedev@gmail.com>
2021-08-10 22:30:27 +0100

    prettify

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,49 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby'
 import * as styles from './../styles/footer.module.css'
-import { toTitleCase } from './../utils';
+import { toTitleCase } from '../utils'
 
 const FooterBar = (props) => {
-	const {site} = useStaticQuery(graphql`
-	query {
-		site {
-			siteMetadata {
-				title
-				author {
-					socialProfiles {
-						twitter
-						linkedIn
-						github
+	const { site } = useStaticQuery(graphql`
+		query {
+			site {
+				siteMetadata {
+					title
+					author {
+						socialProfiles {
+							twitter
+							linkedIn
+							github
+						}
 					}
 				}
 			}
 		}
-	}
 	`)
 	const currentYear = new Date().getFullYear()
-	
+
 	return (
 		<footer className={styles.footerBar}>
-				<div className={`container flex-centered ${styles.footerInner}`}>
-				<p className={styles.attr}>&copy;{currentYear} {site.siteMetadata.title}</p>
+			<div className={`container flex-centered ${styles.footerInner}`}>
+				<p className={styles.attr}>
+					&copy;{currentYear} {site.siteMetadata.title}
+				</p>
 				<ul className={`${styles.socialLinks} flex-centered`}>
-				{Object.entries(site.siteMetadata.author.socialProfiles).map(([key, value]) => <li key={key}>
-					<a href={value} target='_blank' rel='noopener noreferrer' className={styles.socialLink}>
-					{toTitleCase(key)}
-					</a>
-				</li>)}
+					{Object.entries(site.siteMetadata.author.socialProfiles).map(
+						([key, value]) => (
+							<li key={key}>
+								<a
+									href={value}
+									target='_blank'
+									rel='noopener noreferrer'
+									className={styles.socialLink}>
+									{toTitleCase(key)}
+								</a>
+							</li>
+						)
+					)}
 				</ul>
-				</div>
+			</div>
 		</footer>
 	)
 }
--- src/pages/portfolio/{mdx.slug}.js
+++ src/pages/portfolio/{mdx.slug}.js
@@ -1,58 +1,62 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
-import Layout from '../../components/layout';
+import Layout from '../../components/layout'
 import * as styles from './../../styles/project-template.module.css'
 
-
-const Project = ({data}) => {
-	const {title, overview, liveUrl, gitRepo, featuredImage} = data.mdx.frontmatter
+const Project = ({ data }) => {
+	const { title, overview, liveUrl, gitRepo, featuredImage } =
+		data.mdx.frontmatter
 	const image = getImage(featuredImage)
-	
+
 	return (
 		<Layout>
 			<Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
 			<section>
-			<div className={`container`}>
-				<div className={styles.inner}>
-				<div>
-				<h1>{title}</h1>
-				<p>{overview}</p>
-				<div className={styles.projectLinks}>
-				{liveUrl && <a 
-					href={liveUrl} 
-					target='_blank' rel='noopenner noreferrer'
-					className={styles.projectLink}
-					>
-					<span className={styles.linkIcon}>
-						<FontAwesomeIcon icon={faExternalLinkAlt} />
-					</span>
-					Live Site</a>}
-				{gitRepo && <a 
-					href={gitRepo} 
-					target='_blank' rel='noopenner noreferrer'
-					className={styles.projectLink}
-					>
-					<span className={styles.linkIcon}>
-						<FontAwesomeIcon icon={faExternalLinkAlt} />
-					</span>
-					Source Code</a>}
+				<div className={`container`}>
+					<div className={styles.inner}>
+						<div>
+							<h1>{title}</h1>
+							<p>{overview}</p>
+							<div className={styles.projectLinks}>
+								{liveUrl && (
+									<a
+										href={liveUrl}
+										target='_blank'
+										rel='noopenner noreferrer'
+										className={styles.projectLink}>
+										<span className={styles.linkIcon}>
+											<FontAwesomeIcon icon={faExternalLinkAlt} />
+										</span>
+										Live Site
+									</a>
+								)}
+								{gitRepo && (
+									<a
+										href={gitRepo}
+										target='_blank'
+										rel='noopenner noreferrer'
+										className={styles.projectLink}>
+										<span className={styles.linkIcon}>
+											<FontAwesomeIcon icon={faExternalLinkAlt} />
+										</span>
+										Source Code
+									</a>
+								)}
+							</div>
+						</div>
+						<div>
+							<GatsbyImage image={image} alt={title} />
+						</div>
+						<article className={styles.projectDetails}>
+							<MDXRenderer>{data.mdx.body}</MDXRenderer>
+						</article>
+					</div>
 				</div>
-				</div>
-			<div>
-			<GatsbyImage image={image} alt={title} />
-			</div>
-			<article className={styles.projectDetails}>
-			<MDXRenderer>
-					{data.mdx.body}
-			</MDXRenderer>
-		</article>
-				</div>
-			</div>
 			</section>
 		</Layout>
 	)
@@ -61,25 +65,25 @@ const Project = ({data}) => {
 export default Project
 
 export const query = graphql`
-query ($id: String) {
-  mdx(id: {eq: $id}) {
-    frontmatter {
-      title
-			overview
-			liveUrl
-			gitRepo
-			featuredImage {
-				childImageSharp {
-					gatsbyImageData(width: 1024)
+	query ($id: String) {
+		mdx(id: { eq: $id }) {
+			frontmatter {
+				title
+				overview
+				liveUrl
+				gitRepo
+				featuredImage {
+					childImageSharp {
+						gatsbyImageData(width: 1024)
+					}
 				}
 			}
-    }
-		body
-  }
-	site {
-    siteMetadata {
-      title
-    }
-  }
-}
+			body
+		}
+		site {
+			siteMetadata {
+				title
+			}
+		}
+	}
 `
