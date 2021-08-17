2cc4d1f8fbca69c750742c85285be1e017551241
Solomon Ekrebe <krebedev@gmail.com>
2021-08-17 22:05:49 +0100

    update nav menu bg color

--- src/components/CloseIcon.js
+++ src/components/CloseIcon.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CloseIcon = () => {
 	return (
 		<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16.971 16.971'>
-			<g fill='none' stroke='#e3efff' strokeLinecap='round' strokeWidth='2'>
+			<g fill='none' stroke='#103e7e' strokeLinecap='round' strokeWidth='2'>
 				<path d='M1.414 1.415l14.142 14.142M1.414 15.557L15.556 1.415' />
 			</g>
 		</svg>
