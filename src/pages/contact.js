5470bf36c7942ab6c4f6274fc6fdbf36b1815838
Solomon Ekrebe <krebedev@gmail.com>
2022-12-31 14:08:07 +0100

    removes redundant wrapper

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -105,126 +105,124 @@ const Contact = ({ data }) => {
     <Layout>
       <Helmet title={`Contact | ${siteMetadata.title}`} />
       <section className={styles.contact}>
-        <div className={`container`}>
+        <div className={`${styles.contactInner}`}>
           <h1>Get in touch</h1>
-          <div className={`${styles.contactInner}`}>
-            <p>
-              Thanks for taking the time to reach out. Got any question or an
-              idea?{' '}
-              <a href='mailto:solomon@krebe.dev' className={styles.emailLink}>
-                send me an email
-              </a>{' '}
-              or drop a note in the form below.
-            </p>
+          <p>
+            Thanks for taking the time to reach out. Got any question or an
+            idea?{' '}
+            <a href='mailto:solomon@krebe.dev' className={styles.emailLink}>
+              send me an email
+            </a>{' '}
+            or drop a note in the form below.
+          </p>
 
-            <div className={styles.userEntries}>
-              {formData.name && step > 0 && (
-                <button
-                  onClick={() => setStep(0)}
-                  className={styles.editButton}
-                  disabled={isSubmitting}
-                >
-                  <FontAwesomeIcon icon={faUserAlt} />
-                  <span className={styles.fieldValue}>{formData.name}</span>
-                </button>
-              )}
-              {formData.email && step > 1 && (
-                <button
-                  onClick={() => setStep(1)}
-                  className={styles.editButton}
-                  disabled={isSubmitting}
-                >
-                  <FontAwesomeIcon icon={faEnvelope} />
-                  <span className={styles.fieldValue}>{formData.email}</span>
-                </button>
-              )}
-            </div>
-            <Stepper {...{ step, nextStepHandler, handleSubmit, isSubmitting }}>
-              <div className={styles.formGroup}>
-                <label htmlFor='name'>Name</label>
+          <div className={styles.userEntries}>
+            {formData.name && step > 0 && (
+              <button
+                onClick={() => setStep(0)}
+                className={styles.editButton}
+                disabled={isSubmitting}
+              >
                 <FontAwesomeIcon icon={faUserAlt} />
-                <input
-                  onChange={handleChange}
-                  value={formData.name}
-                  type='text'
-                  name='name'
-                  id='name'
-                  placeholder='Enter your name'
-                  className={`${errorFlash && styles.errorHighlight}`}
-                  disabled={isSubmitting}
-                />
-              </div>
-
-              <div className={styles.formGroup}>
-                <label htmlFor='email'>Email Address</label>
-                <FontAwesomeIcon icon={faEnvelope} />
-                <input
-                  onChange={handleChange}
-                  value={formData.email}
-                  type='email'
-                  placeholder='Enter your email address'
-                  name='email'
-                  id='email'
-                  className={`${errorFlash && styles.errorHighlight}`}
-                  disabled={isSubmitting}
-                />
-              </div>
-
-              <div className={`${styles.formGroup} ${styles.messageGroup}`}>
-                <label htmlFor='message'>Message</label>
-                <FontAwesomeIcon icon={faEdit} className={styles.editIcon} />
-                <textarea
-                  onChange={handleChange}
-                  name='message'
-                  id='message'
-                  value={formData.message}
-                  placeholder='Enter your message'
-                  rows={5}
-                  className={`${errorFlash && styles.errorHighlight}`}
-                  disabled={isSubmitting}
-                />
-              </div>
-            </Stepper>
-            {Boolean(sendStatus) && (
-              <div className={styles.toastNotif}>
-                <div className={styles.toastNotifInner}>
-                  <p>
-                    {sendStatus === 200
-                      ? "I've got your message. I'll get back to you within 24hrs. Thank you."
-                      : 'Sorry, your message could not be sent. Please try again later.'}
-                  </p>
-
-                  <button
-                    className={styles.notifBtn}
-                    onClick={() => setSendStatus('')}
-                  >
-                    OK
-                  </button>
-                </div>
-              </div>
+                <span className={styles.fieldValue}>{formData.name}</span>
+              </button>
             )}
-
-            <div className={styles.socials}>
-              <h2>Let's get social</h2>
-              <p>Connect with me on social media.</p>
-              <ul className={styles.profileLinks}>
-                {Object.entries(siteMetadata.author.socialProfiles).map(
-                  ([key, value]) => (
-                    <li key={key} className={styles.profile}>
-                      <a
-                        href={value}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className={`${styles.profileLink} ${styles[key]}`}
-                      >
-                        <span>{socialIcons[key]}</span>
-                        <span className={styles.networkName}>{key}</span>
-                      </a>
-                    </li>
-                  )
-                )}
-              </ul>
+            {formData.email && step > 1 && (
+              <button
+                onClick={() => setStep(1)}
+                className={styles.editButton}
+                disabled={isSubmitting}
+              >
+                <FontAwesomeIcon icon={faEnvelope} />
+                <span className={styles.fieldValue}>{formData.email}</span>
+              </button>
+            )}
+          </div>
+          <Stepper {...{ step, nextStepHandler, handleSubmit, isSubmitting }}>
+            <div className={styles.formGroup}>
+              <label htmlFor='name'>Name</label>
+              <FontAwesomeIcon icon={faUserAlt} />
+              <input
+                onChange={handleChange}
+                value={formData.name}
+                type='text'
+                name='name'
+                id='name'
+                placeholder='Enter your name'
+                className={`${errorFlash && styles.errorHighlight}`}
+                disabled={isSubmitting}
+              />
             </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor='email'>Email Address</label>
+              <FontAwesomeIcon icon={faEnvelope} />
+              <input
+                onChange={handleChange}
+                value={formData.email}
+                type='email'
+                placeholder='Enter your email address'
+                name='email'
+                id='email'
+                className={`${errorFlash && styles.errorHighlight}`}
+                disabled={isSubmitting}
+              />
+            </div>
+
+            <div className={`${styles.formGroup} ${styles.messageGroup}`}>
+              <label htmlFor='message'>Message</label>
+              <FontAwesomeIcon icon={faEdit} className={styles.editIcon} />
+              <textarea
+                onChange={handleChange}
+                name='message'
+                id='message'
+                value={formData.message}
+                placeholder='Enter your message'
+                rows={5}
+                className={`${errorFlash && styles.errorHighlight}`}
+                disabled={isSubmitting}
+              />
+            </div>
+          </Stepper>
+          {Boolean(sendStatus) && (
+            <div className={styles.toastNotif}>
+              <div className={styles.toastNotifInner}>
+                <p>
+                  {sendStatus === 200
+                    ? "I've got your message. I'll get back to you within 24hrs. Thank you."
+                    : 'Sorry, your message could not be sent. Please try again later.'}
+                </p>
+
+                <button
+                  className={styles.notifBtn}
+                  onClick={() => setSendStatus('')}
+                >
+                  OK
+                </button>
+              </div>
+            </div>
+          )}
+
+          <div className={styles.socials}>
+            <h2>Let's get social</h2>
+            <p>Connect with me on social media.</p>
+            <ul className={styles.profileLinks}>
+              {Object.entries(siteMetadata.author.socialProfiles).map(
+                ([key, value]) => (
+                  <li key={key} className={styles.profile}>
+                    <a
+                      href={value}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className={`${styles.profileLink} ${styles[key]}`}
+                    >
+                      <span>{socialIcons[key]}</span>
+                      <span className={styles.networkName}>{key}</span>
+                    </a>
+                  </li>
+                )
+              )}
+            </ul>
           </div>
         </div>
       </section>
