caa1e380edc0b7b774a3b426e0619f4b101db36c
Solomon Ekrebe <krebedev@gmail.com>
2022-12-31 14:06:47 +0100

    updates home-page copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/Layout'
 import Button from '../components/Button'
-import ProjectCards from './../components/ProjectCards'
-import BlogCards from './../components/BlogCards'
-import Cta from './../components/Cta'
 import * as styles from '../pages-styles/index.module.css'
 
 const HomePage = ({ data }) => {
@@ -22,12 +19,29 @@ const HomePage = ({ data }) => {
               <span className={styles.highlight}>{jobTitle}</span>
             </h1>
             <p>
-              I build elegant and efficient frontend products that deliver great
-              user experiences.
+              I build elegant and efficient{' '}
+              <Link to='/portfolio' className={styles.link}>
+                frontend products
+              </Link>{' '}
+              that provide excellent user experiences.
+            </p>
+
+            <p>
+              Every once in a while, I{' '}
+              <Link to='/blog' className={styles.link}>
+                blog
+              </Link>{' '}
+              about tech and fitness. What a mix, you may wonder. But there's
+              even a little more{' '}
+              <Link to='/about' className={styles.link}>
+                about me
+              </Link>
+              .
             </p>
 
             <div className={styles.heroCta}>
-              <Button pathname={'/about'} title={'More About me'} />
+              <p>Am I the droid you're looking for?</p>
+              <Button pathname={'/contact'} title={"Let's talk!"} />
             </div>
           </article>
 
@@ -43,28 +57,6 @@ const HomePage = ({ data }) => {
           </div>
         </div>
       </section>
-      <section>
-        <div className={`container`}>
-          <h2 className={styles.sectionTitle}>Featured Projects</h2>
-          <p>
-            Here's a few of my work with some tools and tech stack I have
-            dabbled with.
-          </p>
-          <ProjectCards projects={data.allMdx.nodes} />
-          <Button pathname={'/portfolio'} title={'View all Projects'} />
-        </div>
-      </section>
-      <section>
-        <div className={`container`}>
-          <h2 className={styles.sectionTitle}>Writing</h2>
-          <p>
-            I'm working on a few articles. Watch this space for my latest posts.
-          </p>
-          <BlogCards />
-          {/* <Button pathname={'/blog'} title={'See all Posts'} /> */}
-        </div>
-      </section>
-      <Cta />
     </Layout>
   )
 }
